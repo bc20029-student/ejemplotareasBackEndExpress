5d5c64ecfe0fcfcee2ce5b5f9fe02ed7e0b46042
bc20029-student <bc20029@ues.edu.sv>
2026-05-17 16:10:28 -0600

    despelegar en railway

--- db/conn.js
+++ db/conn.js
@@ -1,11 +1,8 @@
 const { MongoClient } = require('mongodb');
-const connectionString = process.env.ATLAS_URI;
-const mongoURL = process.env.MONGO_URL || connectionString
 
-const client = new MongoClient(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = process.env.ATLAS_URI;
+
+const client = new MongoClient(uri);
 
 let dbConnection;
 
@@ -13,11 +10,12 @@ module.exports = {
   connectToServer: function (callback) {
     client.connect(function (err, db) {
       if (err || !db) {
+        console.error("❌ Error conectando a Mongo:", err);
         return callback(err);
       }
 
       dbConnection = db.db('ejemplo');
-      console.log('Successfully connected to MongoDB.');
+      console.log('✅ Successfully connected to MongoDB.');
 
       return callback();
     });
